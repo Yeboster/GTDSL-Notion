d8c01c0c82f925cbaef8c2de517bf52b3ad7cd1c
Yeboster <yeboster@gmail.com>
2020-08-10 17:01:00 +0200

    WIP: Refactor code as modules.

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-from typing import *
-from dotenv import load_dotenv
-from notion_api import NotionAPI
+import logging
 from os import getenv
+from typing import *
+
+from dotenv import load_dotenv
+from notion.client import NotionClient
+from notion.collection import Collection
+
+from notion_dsl import Task, decode_dsl
+
+logger = logging.getLogger(__name__)
+
+
+def get_tasks(collection: Collection) -> List[Task]:
+    tasks: List[Task] = []
+    for notion_task in collection.get_rows():
+        properties: Dict[str, str] = notion_task.get_all_properties()
+
+        id = notion_task.id
+        title = notion_task.title
+        task: Task = decode_dsl(id, title, properties)
+
+        logger.debug(task)
+
+        tasks.append(task)
+
+    return tasks
 
 
 if __name__ == '__main__':
-    # Config
+    # Setup
     load_dotenv()
+    log_level = logging.DEBUG if getenv(
+        'ENVIRONMENT', 'development') == 'development' else logging.INFO
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s", level=log_level)
 
     token = getenv("NOTION_TOKEN")
-    client = NotionAPI(token)
-    cv_tasks = client.get_tasks_collection_view()
-    print(cv_tasks)
+    client = NotionClient(
+        token_v2=token, monitor=True, start_monitoring=True, enable_caching=False)
 
-    tasks_views = cv_tasks.views
-    print(tasks_views)
+    inbox_url = getenv("INBOX_URL")
+    tasks_url = getenv("TASKS_URL")
+    projects_url = getenv("PROJECTS_URL")
 
-    scheduled_tasks = client.filter_tasks_with_schedule()
-    print(scheduled_tasks)
-    for task in scheduled_tasks:
-        print(task.values)
-    # TODO:
-    # x- Take all pages
-    # x- Find the tasks one
-    # - List of collections
-    # - Use view next actions
-    # - Get list of all tasks
-    # - Add task in collection tasks
-    # - Polling if there are more tasks: use sorted created_at view
-    # - Add into db
+    inbox_cv = client.get_collection_view(inbox_url)
+    inbox_col = inbox_cv.collection
+
+    tasks = get_tasks(inbox_col)
--- notion_dsl.py
+++ notion_dsl.py
@@ -3,40 +3,52 @@ from dataclasses import dataclass
 from notion.collection import NotionDate
 
 
-TO_INSERT_KEYS: List[str] = ['title',
-                             'project', 'scheduled', 'status', 'context']
+@dataclass
+class Project:
+    """Class to represent notion GTD project"""
+    id: str = None
+    title: str = None
 
 
 @dataclass
 class Task:
-    """Class to represent notion GTD tasks"""
+    """Class to represent notion GTD task"""
     id: str = None
     title: str = None
-    project: str = None
+    project_name: str = None
+    choosen_project: Project = None
     scheduled: NotionDate = None
     status: str = None
     context: str = None
     convert: bool = False
 
+    def apply_properties_from(self, properties: Dict[str, str]) -> None:
+        keys = ['scheduled', 'status', 'context', 'convert']
+        for key in keys:
+            value = properties[key]
+            setattr(self, key, value)
 
-def extract_properties(properties: Dict[str, str], task: Task) -> Task:
-    keys = ['scheduled', 'status', 'context', 'convert']
-    for key in keys:
-        value = properties[key]
-        setattr(task, key, value)
+    def dict_to_insert(self) -> Dict[str, Any]:
+        keys = ['title', 'project', 'scheduled', 'status', 'context']
+        insert_dict: Dict[str, Any] = {}
+        for key in keys:
+            if key == 'project':
+                if prj := self.choosen_project:
+                    insert_dict['project'] = prj.id
+            else:
+                insert_dict[key] = getattr(self, key)
 
-    return task
+        return insert_dict
 
 
 def decode_dsl(id: str, content: str, properties: Dict[str, str]) -> Task:
-    task: Task = Task()
-    task.id = id
+    task: Task = Task(id)
     if (pos := content.find(':')) > -1:
         task.title = content[pos+1:].strip().capitalize()
-        task.project = content[:pos].strip().capitalize()
+        task.project_name = content[:pos].strip().capitalize()
     else:
         task.title = content.strip().capitalize()
 
-    task = extract_properties(properties, task)
+    task.apply_properties_from(properties)
 
     return task
