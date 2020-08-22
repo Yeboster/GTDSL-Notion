74bd19943f6dad4b37d5094e322da959d1b8b3e7
Yeboster <yeboster@gmail.com>
2020-08-22 18:47:21 +0200

    Clean notebook.

--- lib/notebook.py
+++ lib/notebook.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from typing import *
 
 from notion.block import PageBlock, TextBlock
-from notion_dsl import decode_dsl, Task
+from dsl import decode_dsl, Task
 from dotenv import load_dotenv
 from notion.block import PageBlock, TextBlock
 from notion.client import NotionClient
@@ -17,7 +17,8 @@ load_dotenv()
 # %%
 token = getenv("NOTION_TOKEN")
 client = NotionClient(
-    token_v2=token, monitor=True, start_monitoring=True, enable_caching=False)
+    token_v2=token, monitor=True, start_monitoring=True, enable_caching=False
+)
 
 inbox_url = getenv("INBOX_URL")
 tasks_url = getenv("TASKS_URL")
@@ -48,33 +49,3 @@ inbox_col.add_callback(callback)
 
 while True:
     pass
-# %%
-# TODO: Add scheduled tasks to calendar
-# TODO: Add laptop tasks to pomodone
-
-# %%
-scheduled_view_url = 'https://www.notion.so/d16609502fc44cbcb22747c853f549da?v=02ae4d11fe084753a3320ff1f15e3212'
-scheduled_view = client.get_collection_view(scheduled_view_url)
-
-# %%
-# Notion changed default_query param name
-default_filters = scheduled_view.get('query2')['filter']['filters']
-scheduled_view.build_query(filter=default_filters).execute()
-
-
-# %%
-scheduled_view = None
-for view in tasks_views:
-    name: str = view.name
-    print(name)
-    if name.lower() == "scheduled":
-        scheduled_view = view
-# %%
-scheduled_collection = scheduled_view.collection
-
-sched_rows = scheduled_collection.get_rows()
-# %%
-scheduled_tasks = client.filter_tasks_with_schedule()
-print(scheduled_tasks)
-for task in scheduled_tasks:
-    print(task.values)
