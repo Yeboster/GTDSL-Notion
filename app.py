2942a51cf38eb043c260e2dd6a46e9081858e8e3
Yeboster <yeboster@gmail.com>
2020-08-22 15:38:57 +0200

    Create jobs module and WIP delete old converted tasks.

--- app.py
+++ app.py
@@ -8,32 +8,9 @@ from dotenv import load_dotenv
 from notion.client import NotionClient
 from notion.collection import Collection
 
-from lib.dsl import Task, get_tasks
+from lib.jobs import process_inbox_tasks, delete_old_converted_tasks
 
 logger = logging.getLogger(__name__)
-
-
-def process_inbox_tasks(inbox_col: Collection, tasks_col: Collection, projects_col: Collection):
-    logging.info("--- PROCESSING TASKS ----")
-    tasks: List[Task] = get_tasks(inbox_col)
-    logging.info(f"List inbox tasks {tasks}")
-
-    for task in tasks:
-        if task.convert:
-            task.assign_or_create_project_into(projects_col)
-
-            logging.warning(f"Creating new task for {task}")
-
-            task.insert_into(tasks_col)
-
-            logging.debug("Running post creation actions")
-
-            inbox_block = client.get_block(task.id)
-            task.post_creation_action(inbox_block)
-
-            logging.warning("Task correctly inserted.")
-
-    logging.info("--- PROCESSING FINISHED ---")
 
 
 if __name__ == '__main__':
@@ -65,9 +42,12 @@ if __name__ == '__main__':
     tasks_cv = client.get_collection_view(tasks_url)
     tasks_col: Collection = tasks_cv.collection
 
-    process_inbox_tasks(inbox_col, tasks_col, projects_col)
-    schedule.every(1).minutes.do(process_inbox_tasks,
-                                 inbox_col, tasks_col, projects_col)
+    # Jobs
+    get_block = client.get_block
+    process_inbox_tasks(inbox_col, tasks_col, projects_col, get_block)
+    schedule.every(2).minutes.do(process_inbox_tasks,
+                                 inbox_col, tasks_col, projects_col, get_block)
+    schedule.every(1).day.do(delete_old_converted_tasks, inbox_col)
 
     while True:
         schedule.run_pending()
---
+++ lib/jobs.py
@@ -0,0 +1,39 @@
+import logging
+from typing import *
+from notion.collection import Collection
+from .dsl import Task, get_tasks
+
+logger = logging.getLogger(__name__)
+
+
+def process_inbox_tasks(inbox_col: Collection, tasks_col: Collection, projects_col: Collection, get_block: Callable):
+    logging.info("--- PROCESSING TASKS ----")
+    tasks: List[Task] = get_tasks(inbox_col)
+    logging.info(f"List inbox tasks {tasks}")
+
+    for task in tasks:
+        if task.convert:
+            task.assign_or_create_project_into(projects_col)
+
+            logging.warning(f"Creating new task for {task}")
+
+            task.insert_into(tasks_col)
+
+            logging.debug("Running post creation actions")
+
+            inbox_block = get_block(task.id)
+            task.post_creation_action(inbox_block)
+
+            logging.warning("Task correctly inserted.")
+
+    logging.info("--- PROCESSING FINISHED ---")
+
+
+def delete_old_converted_tasks(inbox_col: Collection):
+    """Delete old converted tasks job
+    """
+    tasks: List[Task] = get_tasks(inbox_col)
+    for task in tasks:
+        if not task.convert:
+            pass
+            # Check if already converted, if so, delete
